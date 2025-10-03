859015f1f2e60ebf2fbf2c5dfcfc6299452a2a3e
Sonia Debora Napitupulu <adoradebora99@gmail.com>
2025-10-03 15:14:48 +0700

    Pertemuan-Laravel1: Intalasi, Konfigurasi dan Halaman Pertama Kita

---
+++ LaraPress/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Tentang Kami </title>
+    </head>
+
+    <body>
+        <h1> Tentang LaraPress </h1>
+        <p> Larapress adalah sebuah proyek bog sederhana yang dibuat untuk mempelajari dasar-dasar framework Laravel 12 </p>
+
+        <hr>
+        <a href="/"> Kembali ke Halaman Utama </a>
+        <!-- Penambahan nav ke halaman kontak -->
+        <br>
+        <a href="/kontak"> Hubungi kami </a> 
+    </body>
+</html>
---
+++ LaraPress/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang-kami', function () {
+    return view('about');
+});
+
+Route::get('/kontak', function () {
+    return view('contact');
+});
